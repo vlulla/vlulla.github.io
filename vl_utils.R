7af1e6809502efdc50133e116ad440e23b91dc71
Vijay Lulla <vijaylulla@gmail.com>
2023-12-01 18:47:07 -0500

    Fixed date range generation

--- vl_utils.R
+++ vl_utils.R
@@ -584,5 +584,5 @@ generate_random_date_range <- function(start_date=as.Date('2000-01-01'), end_dat
   ## R> generate_random_date_range(as.Date('2000-01-01'),as.Date('2022-12-31'), as.integer(sample(365,1))) ## much more useful...imo
   ## R> generate_random_date_range(as.Date('2000-01-01'),as.Date('2022-12-31'), as.integer(sample(c(365,366),1,prob=c(3/4,1/4)))) ## even more interesting
   stopifnot(class(start_date)==class(Sys.Date()), class(end_date)==class(Sys.Date()), is.integer(num_days), end_date > start_date, end_date - start_date >= num_days)
-  with(list(start_dt=sample(seq(start_dt,end_date-num_days,by='1 day'),1)),seq(start_dt, start_dt+num_days,by='1 day'))
+  with(list(start_dt=sample(seq(start_dt,end_date-num_days,by='1 day'),1)),seq(start_dt, start_dt+num_days-1,by='1 day'))
 }
