fdfa535b8fdff84b9c59ae5f55c5ed720d05e736
Vijay Lulla <vijay_lulla@sweetwater.com>
2024-08-29 09:57:58 -0400

    Modifications to make colDetails work for columns which report more than one class
    `IDate` and `ordered` types report more than one class which breaks the `colDetails` function!

--- vl_utils.R
+++ vl_utils.R
@@ -331,12 +331,12 @@ colDetails <- function(DF) {
   num_uniq <- sapply(DF, num_unique)
   DFM <- copy(DF)
   for(i in seq_len(ncol(DF))) {
-    if(class(DF[[i]]) %in% c("factor","character")) { DFM[[i]] <- as.numeric(rep(NA,nrow(DFM))) }
+    if(any(class(DF[[i]]) %in% c("factor","character"))) { DFM[[i]] <- as.numeric(rep(NA,nrow(DFM))) }
   }
   colstats <- function(x, na.rm=TRUE, digits=3L) {
     ## idea from McElreath's rethinking::precis function
     stats <- if(is.numeric(x)) {
-      c(round(mean(x,na.rm=na.rm),digits=digits), round(sd(x,na.rm=na.rm),digits=digits), round(quantile(x,probs=c(0.055,0.945)),digits=digits))
+      c(round(mean(x,na.rm=na.rm),digits=digits), round(sd(x,na.rm=na.rm),digits=digits), round(quantile(x,probs=c(0.055,0.945),na.rm=na.rm),digits=digits))
     } else {
       c(NA, NA, NA, NA)
     }
